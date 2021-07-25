be488da0d69d6020aab81aacbcd99a15c1d8aef7
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-07-26 00:25:55 +0530

    fixed some issue

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 function ToDoList(props) {
   const [text, setText] = useState("");
